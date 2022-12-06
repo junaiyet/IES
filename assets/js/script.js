217208479c36125eb08118020d0e935e9f98e745
junaiyet <junaiyetmia470@gmail.com>
2022-12-07 00:19:34 +0600

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,3 +69,27 @@ $('.bank-area-slider').slick({
 
 // herp-slider end
 // backtotop start
+
+
+$(function () {
+  if ( $('html').hasClass('csstransforms3d') ) {
+    $('.artGroup').removeClass('slide').addClass('flip');
+    $('.artGroup.flip').on('mouseenter',
+      function () {
+        $(this).find('.artwork').addClass('theFlip');
+      });
+    $('.artGroup.flip').on('mouseleave',
+      function () {
+        $(this).find('.artwork').removeClass('theFlip');
+      });
+  } else {
+    $('.artGroup').on('mouseenter',
+      function () {
+        $(this).find('.detail').stop().animate({bottom:0}, 500, 'easeOutCubic');
+      });
+    $('.artGroup').on('mouseleave',
+      function () {
+        $(this).find('.detail').stop().animate({bottom: ($(this).height() + -1) }, 500, 'easeOutCubic');
+      });
+  }
+});
